ad6c3254b21c7cfc154b8f8633ef19ff26f75eb1
FranHasenauer <franciscohasenauer@gmail.com>
2023-07-16 06:06:26 -0300

    con el login y pedidos

--- src/java/Persistencia.java
+++ src/java/Persistencia.java
@@ -3,38 +3,55 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Persistencia {
 
-    private Connection cn; //Conectar a la base de datos
-    private ResultSet rs;  //Recibir datos de la base de datos
-    private PreparedStatement ps; // ejecutar datos de la base de datos
+    //Retorna la conexion
+    private Connection cn;
+    //Recibe los datos que vienen de la bbdd
+    private ResultSet rs;
+    //Recibe la orden de la busqueda 
+    private PreparedStatement ps;
+    
+    private ResultSetMetaData rsm;
 
     public String servidor, bbdd, usuario, clave, ejecutar;
 
     public Connection conectarse() {
-
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-        
-        servidor = "localhost";
-bbdd="tp_final";
-        usuario="root";
-        clave="";
+            Class.forName("com.mysql.jdbc.driver");
+
+            servidor = "localhost:8080/";
+            bbdd = "tp_final";
+            usuario = "root";
+            clave = "";
+
+            cn = DriverManager.getConnection("jdbc:mysql://" + servidor + bbdd+"?autoReconnect=true&useSSL=false" + usuario + clave);
+
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Persistencia.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        try {
-            cn = DriverManager.getConnection("jdbc:mysql://" +  servidor + bbdd+usuario+clave);
         } catch (SQLException ex) {
             Logger.getLogger(Persistencia.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return null;
 
-    
+        return cn;
+
     }
-
+//.-.-.-.-..--.-.-..-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--..
+    public ResultSet consultaSql(String busqueda){
+        try {
+            ps=conectarse().prepareStatement(busqueda);
+      
+        rs=ps.executeQuery();
+        rsm=rs.getMetaData();
+          } catch (SQLException ex) {
+            Logger.getLogger(Persistencia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+    
 }
